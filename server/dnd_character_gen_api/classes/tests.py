123271a6853999b602b03dddf38a30958fe1456c
rhawkens99 <richard@deyoungzone.com>
2023-07-02 18:16:42 -0700

    write first test

--- server/dnd_character_gen_api/classes/tests.py
+++ server/dnd_character_gen_api/classes/tests.py
@@ -1,3 +1,18 @@
+from django.forms import ValidationError
 from django.test import TestCase
+from .models import Classes
 
-# Create your tests here.
+
+class ClassesModelTests(TestCase):
+    def test_hit_die_is_invalid_option(self):
+        """
+        Expect that the passed value of 'dice' is invalid
+        """
+        die = "dice"
+        new_class = Classes(
+            name="Name",
+            description="description",
+            hit_die=die,
+            primary_ability="Strength",
+        )
+        self.assertRaises(ValidationError, new_class.save, new_class)
